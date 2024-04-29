3032183fc10222a30e94774cb2bcfccc058f8069
Matt Deuschle <mdeuschle@gmail.com>
2024-04-28 19:18:56 -0700

    first commit

---
+++ MattStashApp/MattStashApp/MattStashAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MattStashAppApp.swift
+//  MattStashApp
+//
+//  Created by Matt Deuschle on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MattStashAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
